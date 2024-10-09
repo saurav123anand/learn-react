bd79a881afae2025350ce9451fa856d397813b1b
GeekySaurav <65071308+saurav123anand@users.noreply.github.com>
2024-10-09 22:42:47 +0530

    Update FirebaseConfig.js

--- expense_tracker_add_update/src/FirebaseConfig.js
+++ expense_tracker_add_update/src/FirebaseConfig.js
@@ -7,13 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDahEcfxNI1e1WSA84k8c2znhgtqX_4SRk",
-  authDomain: "react-demo-3b1a4.firebaseapp.com",
-  projectId: "react-demo-3b1a4",
-  storageBucket: "react-demo-3b1a4.appspot.com",
-  messagingSenderId: "1016798441382",
-  appId: "1:1016798441382:web:ef615cffe2ae421cb1e967",
-  measurementId: "G-BSB5R1EB2F"
+  
 };
 
 // Initialize Firebase
